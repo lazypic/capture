6dcb492b7af0c283123b559042bf6acccfa5a39f
kim hanwoong <khw7096@gmail.com>
2018-10-25 00:47:40 +0900

    옵션 수정함.

--- cam.sh
+++ cam.sh
@@ -1,2 +1,2 @@
-FILE="$(date +%Y%m%dT%H%M%S).mkv"
-ffmpeg -f avfoundation -i "1:0" ~/desktop_$FILE & ffmpeg -f avfoundation -i "0:0" ~/face_$FILE &
+FILE="$(date +%Y%m%dT%H%M%S)"
+ffmpeg -f avfoundation -i "1:0" ~/desktop_$FILE.mkv & ffmpeg -f avfoundation -i "0:0" -c:v prores ~/face_$FILE.mov &
--- facecam.sh
+++ facecam.sh
@@ -1,2 +1,2 @@
-FILE="face_$(date +%Y%m%dT%H%M%S).mkv"
-ffmpeg -f avfoundation -i "0:0" ~/$FILE
+FILE="face_$(date +%Y%m%dT%H%M%S).mov"
+ffmpeg -f avfoundation -i "0:0" -c:v prores ~/$FILE
